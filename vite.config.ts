e589e4e5fe78050893a81fe36b1925ed0063eb77
secretshardul <aeershardul@gmail.com>
2023-08-22 15:59:12 +0400

    fix: Remove base
    Not needed when URL is set up

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,4 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-
-  // Needed for Github pages- https://vitejs.dev/guide/static-deploy.html#github-pages
-  base: '/podl-webapp/'
 })
